d5b29e84a109c9947b7a1e496d901d4d5b3e4773
vvguoliang <qin.guoliang@shaxiaoseng.com>
2016-08-12 15:21:31 +0800

    提交图片

--- MyKotlin/app/src/main/kotlin/MainActivity.kt
+++ MyKotlin/app/src/main/kotlin/MainActivity.kt
@@ -1,35 +1,35 @@
 package com.example.mykotlin
 
-import android.content.Intent
+import android.app.Activity
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
 
+    val webviewval = "https://pic2.zhimg.com/8a3ab4d0e9f254bbe6827c11296cbdfd_b.png"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        tv.text = "Hello MyKotlin"
-        tv.textSize = 50.0f
-
-        button.setOnClickListener {
-            val i = Intent(this@MainActivity, MyActivity::class.java)
-            i.putExtra("text", "阿奎罗大大大大开")
-            startActivityForResult(i, 0x001)
-        }
-
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        val textdata = data?.getStringExtra("text") as String
-        if (requestCode == 0x001) {
-            text.text = textdata
-            return
-        }
-
+//        tv.text = "Hello MyKotlin"
+//        tv.textSize = 50.0f
+//
+//        button.setOnClickListener {
+//            val i = Intent(this@MainActivity, MyActivity::class.java)
+//            i.putExtra("text", "阿奎罗大大大大开")
+//            startActivityForResult(i, 0x001)
+//        }
+//
+//    }
+//
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        super.onActivityResult(requestCode, resultCode, data)
+//
+//        val textdata = data?.getStringExtra("text") as String
+//        if (requestCode == 0x001) {
+//            text.text = textdata
+//            return
+//        }
+        webview.loadUrl(webviewval)
     }
 }
 
